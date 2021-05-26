a5b08bb684159a567c20de64f4d0064afe348e8a
al ex <fedotenko.aleksey@gmail.com>
2021-05-27 00:51:55 +0300

    Enable scheduling

--- src/main/java/com/sendmequiz/quizemailverifiersecond/QuizEmailVerifierSecondApplication.java
+++ src/main/java/com/sendmequiz/quizemailverifiersecond/QuizEmailVerifierSecondApplication.java
@@ -2,8 +2,10 @@ package com.sendmequiz.quizemailverifiersecond;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class QuizEmailVerifierSecondApplication {
 
 	public static void main(String[] args) {
